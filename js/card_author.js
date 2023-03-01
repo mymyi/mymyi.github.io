578d5f103fbdf34bc725d9f1f306e5f576e988d0
zhaoxiaoxixi <28392828@163.com>
2023-03-01 17:40:32 +0800

    Site updated: 2023-03-01 17:40:32

--- js/card_author.js
+++ js/card_author.js
@@ -2,12 +2,12 @@ var card_author = {
   getTimeState: function() {
       var e = (new Date).getHours()
         , t = "";
-      return 0 <= e && e <= 5 ? t = "晚安😴" : 5 < e && e <= 10 ? t = "早上好👋" : 10 < e && e <= 14 ? t = "中午好👋" : 14 < e && e <= 18 ? t = "下午好👋" : 18 < e && e <= 24 && (t = "晚上好👋"),
+      return 0 <= e && e <= 5 ? t = "朋友你好呀👋" : 5 < e && e <= 10 ? t = "朋友早上好呀👋" : 10 < e && e <= 14 ? t = "朋友中午好呀👋" : 14 < e && e <= 18 ? t = "朋友下午好呀👋" : 18 < e && e <= 24 && (t = "朋友晚上好呀👋"),
       t
   },
   sayhi: function() {
       var e = document.getElementById("author-info__sayhi");
-      e && (e.innerHTML = card_author.getTimeState() + "！我是")
+      e && (e.innerHTML = card_author.getTimeState() + "")
   },
 }
 card_author.sayhi();
