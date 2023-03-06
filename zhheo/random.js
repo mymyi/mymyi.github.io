66a6375a916e4c947d7d1df5243f897c0ed6e7f3
zhaoxiaoxixi <28392828@163.com>
2023-03-06 23:56:11 +0800

    Site updated: 2023-03-06 23:56:11

--- zhheo/random.js
+++ zhheo/random.js
@@ -1,1 +1,1 @@
-var posts=["post/3e406604.html","post/36986c4c.html","post/9f9cd7c0.html","post/5f1a0e6f.html","post/undefined.html","post/421b8f4d.html"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["post/3e406604.html","post/9f9cd7c0.html","post/bbb8371.html","post/36986c4c.html","post/undefined.html","post/431f1634.html","post/5f1a0e6f.html","post/421b8f4d.html"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
